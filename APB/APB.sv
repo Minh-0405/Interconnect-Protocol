626f68756a68073bce3d1ab71dc737096e50abaa
Duc Minh <minh.du040506@hcmut.edu.vn>
2026-09-03 10:05:40 +0700

    complete APB wait state

--- APB/APB.sv
+++ APB/APB.sv
@@ -10,21 +10,28 @@ interface apb_if #(
     logic pwrite ;
     logic [ADDR_WIDTH-1:0] paddr ;
     logic [WDATA_WIDTH-1:0] pwdata ;
+    logic [WDATA_WIDTH/8-1:0] pstrb ;
     logic [RDATA_WIDTH-1:0] prdata ;
     logic pready ;
+    logic pslverr ;
 
     modport master(
-        output psel, penable, pwrite, paddr, pwdata,
-        input prdata, pready
+        output psel, penable, pwrite, paddr, pwdata, pstrb,
+        input prdata, pready, pslverr
     );
 
     modport slave(
-        input psel, penable, pwrite, paddr, pwdata,
-        output prdata, pready
+        input psel, penable, pwrite, paddr, pwdata, pstrb,
+        output prdata, pready, pslverr
     );
 endinterface
 
-module APB_Master(
+module APB_Master #(
+    parameter int ADDR_WIDTH = 12,
+    parameter int WDATA_WIDTH = 32,
+    parameter int RDATA_WIDTH = 8
+)
+(
     input  logic clk, rst_n,
     input  logic cmd_vld,
     input  logic cmd_write,
@@ -32,9 +39,10 @@ module APB_Master(
     input  logic [3:0] cmd_len,
     output logic ack,
     output logic done,
-    input  logic [11:0] cmd_addr,
-    input  logic [31:0] cmd_wdata,
-    output logic [7:0] cmd_rdata,
+    input  logic [ADDR_WIDTH-1:0] cmd_addr,
+    input  logic [WDATA_WIDTH-1:0] cmd_wdata,
+    input  logic [WDATA_WIDTH/8-1:0] cmd_strb,
+    output logic [RDATA_WIDTH-1:0] cmd_rdata,
     apb_if.master apb_m
 );
     typedef enum logic [2:0] {
@@ -42,10 +50,28 @@ module APB_Master(
         SETUP = 3'b010,
         ACCESS = 3'b100
     } state_t ;
-    state_t state ;
+    state_t state, next_state ;
 
-    logic transfer_free ;
     logic [3:0] counter ;
+    always_comb begin
+        next_state = state ;
+        unique case(state)
+            IDLE: begin
+                if(!done & cmd_vld) next_state = SETUP ;
+                else next_state = state ;
+            end
+            SETUP: next_state = ACCESS ;
+            ACCESS: begin
+                if(!apb_m.pready) next_state = state ;
+                else if((!cmd_mode) | (cmd_mode & (!(counter ^ (cmd_len+1)))))
+                    next_state = IDLE ;
+                else
+                    next_state = SETUP ;
+            end
+            default: ;
+        endcase
+    end
+
     always_ff @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
             state <= IDLE ;
@@ -53,73 +79,108 @@ module APB_Master(
             apb_m.penable <= 1'b0 ;
             ack <= 1'b0 ;
             done <= 1'b0 ;
-            transfer_free <= 1'b1 ;
         end
         else begin
-            unique case(state)
+            state <= next_state ;
+            unique case(next_state)
                 IDLE: begin
                     apb_m.psel <= 1'b0 ;
                     apb_m.penable <= 1'b0 ;
                     counter <= 'b0 ;
-                    transfer_free <= 1'b1 ;
-                    if(transfer_free & cmd_vld) state <= SETUP ;
-                    else state <= IDLE ;
+                    ack <= 1'b0 ;
+                    done <= 1'b0 ;
                 end
                 SETUP: begin
-                    transfer_free <= 1'b0 ;
+                    ack <= 1'b0 ;
                     apb_m.psel <= 1'b1 ;
                     apb_m.penable <= 1'b0 ;
                     apb_m.paddr <= (cmd_addr + (counter << 2)) ;
                     apb_m.pwrite <= cmd_write ;
                     apb_m.pwdata <= cmd_wdata ;
+                    apb_m.pstrb <= cmd_strb ;
                     counter <= counter + 1 ;
-                    state <= ACCESS ;
                 end
                 ACCESS: begin
                     apb_m.penable <=  1'b1 ;
-                    if(!cmd_mode | (cmd_mode & (!(counter ^ (cmd_len+1)))))
-                        state <= IDLE ;
-                    else state <= SETUP ;
                 end
                 default: ;
             endcase
-            cmd_rdata <= (apb_m.pready & !cmd_write)? apb_m.prdata : 'bx ;
             ack <= apb_m.pready ;
             done <= (!cmd_mode & apb_m.pready) |
                              (cmd_mode & (!(counter ^ (cmd_len+1)) & apb_m.pready)) ;
+            if(apb_m.pready & !cmd_write) cmd_rdata <= apb_m.prdata ;
         end
     end
 endmodule
 
-module APB_Slave(
+module APB_Slave #(
+    parameter int ADDR_WIDTH = 12,
+    parameter int WDATA_WIDTH = 32,
+    parameter int RDATA_WIDTH =8
+)
+(
     input  logic clk, rst_n,
     apb_if.slave apb_s
 );
-    logic [7:0] regs [4096] ;
-    logic [11:0] addr_reg ;
-    logic [31:0] wdata ;
+    logic [RDATA_WIDTH-1:0] regs [2**(ADDR_WIDTH-1)] ;
+    logic [RDATA_WIDTH-1:0] slow_regs [2**(ADDR_WIDTH-1)] ;
+    logic slow_regs_en [2**(ADDR_WIDTH-1)] ;
 
-    always_comb begin
-        addr_reg = (apb_s.psel)? apb_s.paddr : 'bx ;
-        wdata = (apb_s.psel)? apb_s.pwdata : 'bx ;
+    logic [10:0] counter ;
+    always_ff @(posedge clk, negedge rst_n) begin
+        if(!rst_n) counter <= 'b0 ;
+        else begin
+            if(slow_regs_en[apb_s.paddr[ADDR_WIDTH-2:0]]) counter <= 'b0 ;
+            else if(apb_s.penable & apb_s.paddr[ADDR_WIDTH-1])
+                counter <= counter +1 ;
+        end
     end
-
-    always_comb begin
-        apb_s.prdata = (apb_s.penable & !apb_s.pwrite)? regs[addr_reg][7:0] : 'bx ;
-        apb_s.pready = (apb_s.penable)? 1'b1 : 1'b0 ; // no wait state
-    end
+    genvar i ;
+    generate;
+        for(i=1 ; i <= 1<<(ADDR_WIDTH-1) ; i++)
+        begin : g_en
+            assign slow_regs_en[i-1] = (counter == i) ;
+        end
+    endgenerate
 
     always_ff @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
-            for(int i=0 ; i<4096 ; i++) begin
-                regs[i] <= 32'b0 ;
+            for(int i=0 ; i < 1<<(ADDR_WIDTH-1) ; i++) begin
+                regs[i] <= 'b0 ;
+                slow_regs[i] <= 'b0 ;
             end
         end
-        else if(apb_s.penable & apb_s.pwrite) begin
-            regs[addr_reg] <= wdata[7:0] ;
-            regs[addr_reg+1] <= wdata[15:8] ;
-            regs[addr_reg+2] <= wdata[23:16] ;
-            regs[addr_reg+3] <= wdata[31:24] ;
+        else begin
+             if(apb_s.penable & apb_s.pwrite & !apb_s.paddr[ADDR_WIDTH-1]) begin
+                for(int i=0 ; i< WDATA_WIDTH/8 ; i++) begin
+                    if(apb_s.pstrb[i])
+                        regs[apb_s.paddr + i] <= apb_s.pwdata[i*RDATA_WIDTH +: RDATA_WIDTH] ;
+                end
+            end
+            if(slow_regs_en[apb_s.paddr[ADDR_WIDTH-2:0]]) begin
+                for(int i=0 ; i<WDATA_WIDTH/8 ; i++) begin
+                    if(apb_s.pstrb[i])
+                        slow_regs[apb_s.paddr[ADDR_WIDTH-2:0]+i] <= apb_s.pwdata[i*RDATA_WIDTH +: RDATA_WIDTH] ;
+                end
+            end
+        end
+    end
+
+    always_comb begin
+        apb_s.prdata = 'b0 ;
+        apb_s.pready = 1'b0 ;
+        apb_s.pslverr = 1'b0 ;
+        if(apb_s.penable) begin
+            if(!apb_s.paddr[ADDR_WIDTH-1]) begin
+                apb_s.pready = 1'b1 ;
+                if(!apb_s.pwrite) apb_s.prdata = regs[apb_s.paddr] ;
+            end
+            else begin
+                if(slow_regs_en[apb_s.paddr[ADDR_WIDTH-2:0]]) begin
+                    if(!apb_s.pwrite) apb_s.prdata = slow_regs[apb_s.paddr[ADDR_WIDTH-2:0]] ;
+                    apb_s.pready = 1'b1 ;
+                end
+            end
         end
     end
 endmodule
--- APB/APB_tb.sv
+++ APB/APB_tb.sv
@@ -1,16 +1,29 @@
 `timescale 1ns/1ps
 
 module APB_tb();
+    localparam int ADDR = 12 ;
+    localparam int WDATA = 32 ;
+    localparam int RDATA = 8 ;
+
     logic clk, rst_n ;
     logic cmd_vld, cmd_write, cmd_mode ;
     logic [3:0] cmd_len ;
     logic ack, done ;
-    logic [11:0] cmd_addr ;
-    logic [31:0] cmd_wdata ;
-    logic [7:0] cmd_rdata ;
-    apb_if if_bus() ;
+    logic [ADDR-1:0] cmd_addr ;
+    logic [WDATA-1:0] cmd_wdata ;
+    logic [RDATA-1:0] cmd_rdata ;
+    logic [WDATA/8-1:0] cmd_strb ;
+    apb_if #(.ADDR_WIDTH(ADDR),
+            .RDATA_WIDTH(RDATA),
+            .WDATA_WIDTH(WDATA)
+    ) if_bus() ;
 
-    APB_Master master(
+    APB_Master #(
+        .ADDR_WIDTH(ADDR),
+        .WDATA_WIDTH(WDATA),
+        .RDATA_WIDTH(RDATA)
+    ) master (
+
         .clk(clk),
         .rst_n(rst_n),
         .cmd_vld(cmd_vld),
@@ -22,10 +35,15 @@ module APB_tb();
         .cmd_addr(cmd_addr),
         .cmd_wdata(cmd_wdata),
         .cmd_rdata(cmd_rdata),
+        .cmd_strb(cmd_strb),
         .apb_m(if_bus.master)
     );
 
-    APB_Slave slave(
+    APB_Slave #(
+        .ADDR_WIDTH(ADDR),
+        .WDATA_WIDTH(WDATA),
+        .RDATA_WIDTH(RDATA)
+    ) slave (
         .clk(clk),
         .rst_n(rst_n),
         .apb_s(if_bus.slave)
@@ -48,9 +66,10 @@ module APB_tb();
         cmd_vld = 1 ;
         cmd_write = 1 ;
         cmd_mode = 0 ;
-        cmd_addr = 12'h4 ;
+        cmd_addr = 12'h804 ;
         cmd_wdata = 32'hABC ;
-        clock(4) ;
+        cmd_strb = 4'b0001 ;
+        clock(9) ;
         cmd_vld = 0 ;
         cmd_write = 0 ;
         clock(1) ;
@@ -58,28 +77,24 @@ module APB_tb();
         cmd_vld = 1 ;
         cmd_write = 1 ;
         cmd_mode = 1 ;
-        cmd_len = 4'd3 ;
-        cmd_addr = 12'h4 ;
+        cmd_len = 4'd1 ;
+        cmd_addr = 12'h800 ;
         cmd_wdata = 32'hCBA ;
-        clock(10) ;
+        cmd_strb = 4'b1111 ;
+        clock(12) ;
         cmd_vld = 0 ;
         cmd_write = 0 ;
-        clock(1) ;
-        // single read test
-        cmd_vld = 1 ;
-        cmd_write = 0 ;
-        cmd_mode = 0 ;
-        cmd_addr = 12'h4 ;
-        clock(4) ;
-        cmd_vld = 0 ;
         clock(1) ;
         // burst read test
+        slave.slow_regs[0] = 8'h44 ;
+        slave.slow_regs[4] = 8'h55 ;
+        slave.slow_regs[8] = 8'h66 ;
         cmd_vld = 1 ;
         cmd_write = 0 ;
         cmd_mode = 1 ;
         cmd_len = 4'd2 ;
-        cmd_addr = 12'h8 ;
-        clock(8) ;
+        cmd_addr = 12'h800 ;
+        clock(23) ;
         cmd_vld = 0 ;
         cmd_write = 0 ;
         clock(2) ;
